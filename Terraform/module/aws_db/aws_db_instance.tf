260294d914be6cc656bb823d68864916ba702626
renan.ecm@hotmail.com <renan.ecm@hotmail.com>
2025-03-22 11:31:40 -0300

    Adiciona arquivos iniciais

--- Terraform/module/aws_db/aws_db_instance.tf
+++ Terraform/module/aws_db/aws_db_instance.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "postgres_mcking_instance_rds" {
   tags = {
     Name = "postgres-mcking-instance-rds"
   }
-  
+
   depends_on = [
     aws_security_group.mcking_security_group_rds,
     aws_db_subnet_group.subnet_group_rds
